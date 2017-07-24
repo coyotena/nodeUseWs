38d47bd01af32543da3fd469c1b9daadf0268a8c
coyotena <shashahuo@163.com>
2017-07-24 13:36:09 +0800

    ajax基础

--- js/8ajax/1clock.js
+++ js/8ajax/1clock.js
@@ -4,10 +4,17 @@
 var http = require("http");
 var mime = require("mime");
 var url = require("url");
+var fs = require("fs");
 var server = http.createServer(function(request, response){
     var urlObj = url.parse(request.url,true);
-    response.writeHead(200, {"Content-Type":mime.lookup(request.url)+";charset=utf-8"});
-    if(urlObj.pathname == "/"){}
+    response.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+    if(urlObj.pathname == "/"){
+        fs.readFile("./clock.html",function(err,data){
+            response.end(data);
+        });
+    }else if(urlObj.pathname == "/clock"){
+        response.end(new Date().toLocaleString());
+    }
 
 });
 server.listen(8080,"localhost");
