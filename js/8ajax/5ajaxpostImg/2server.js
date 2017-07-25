d2a2ff6a378531e19841e99e5a33f8981c2aa93c
coyotena <shashahuo@163.com>
2017-07-25 16:23:22 +0800

    接收客户端传过来的文件，使用formidable模块

---
+++ js/8ajax/5ajaxpostImg/2server.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 2017-7-25.
+ */
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var formidable = require("formidable");
+var util = require("util");//工具模块
+var querystring = require("querystring");
+var server = http.createServer(function(request, response){
+    response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+    var urlObj = url.parse(request.url,true);
+    var pathname = urlObj.pathname;
+    if(pathname == "/"){
+        fs.readFile("./2form.html",function(err,data){
+            if(!err){
+                response.end(data);
+            }
+        });
+    }else if(pathname == "/reg2"){
+        //构建一个解析器
+        var form = new formidable.IncomingForm();
+        /**
+         * 参数介绍：
+         * err:错误信息
+         * fields:普通input，包含text等
+         * files:文件
+         */
+        //用解析器解析请求体
+        //把非file的input放在fields里
+        //把文件类型的元素放在files里
+        form.parse(request, function(err, fields, files) {
+            response.writeHead(200, {'content-type': 'text/plain'});
+            response.write('received upload:\n\n');
+            //inspect是把对象转成字符串
+            console.log(util.inspect({fields: fields, files: files}));
+            response.end(util.inspect({fields: fields, files: files}));
+        });
+    }
+
+});
+
+server.listen(8080,"localhost");
