0d8dcfbd48a95e9aa33963f63d397b98eb0f15b8
coyotena <shashahuo@163.com>
2017-07-25 10:13:37 +0800

    发送post请求

--- js/8ajax/4ajaxPost/server.js
+++ js/8ajax/4ajaxPost/server.js
@@ -19,11 +19,14 @@ var server = http.createServer(function(request, response){
     }else if(pathname == "/reg"){
         var result = "";
         //请求体接收数据
+        //当读到客户端提交过来的数据时会触发data事件，然后调用回调函数
         request.on("data",function(data){
             result += data;
         });
         request.on("end",function(){
+            //把查询字符串转成对象
             var obj = querystring.parse(result);
+            //发送响应
             console.log(obj);
             response.end("ok");
         });
