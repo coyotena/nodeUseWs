9fc778f4fa2db30cbf1aef91a4b834affffefb4e
coyotena <shashahuo@163.com>
2017-08-02 14:17:05 +0800

    express配置路由，启动服务器

---
+++ js/14express/1express.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017-8-2.
+ * 1.安装
+ * 2.使用
+ */
+//加载express
+var express = require("express");
+//获取配置对象
+var app = express();//app其实是一个配置对象
+//配置路由
+//当用户访问/的时候，会执行后面的回调函数
+app.get("/",function(req, res){
+    //send可以自动判断参数类型，自动转换响应信息
+    //并且自动设置Content-Type
+    res.send("hello world");
+});
+//启动服务器
+app.listen(3000);
+
