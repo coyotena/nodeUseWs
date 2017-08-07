2f5d648a07eff38b1243d519b3119ce4ccc2f219
coyotena <shashahuo@163.com>
2017-08-07 11:12:51 +0800

    使用模板

--- js/14express/5template.js
+++ js/14express/5template.js
@@ -3,19 +3,22 @@
  */
 var express = require("express");
 var app = express();
-var ejs = require("ejs");
+var path = require("path");
 /**
  1.动态内容 当前时间
  2.静态内容
  3.动静结合
  */
 //设置配置属性值
+//配置模板引擎
 app.set("view engine","ejs");
 //指定模板存放目录
-app.set("views",process.cwd() + "/views");//process.cwd获取当前的工作目录
+// app.set("views",process.cwd() + "/views");//process.cwd获取当前的工作目录
+//resolve 现货区当前文件的所在绝对目录，然后再拼上后面的路径
+app.set("views",path.resolve("views"));
 
 app.get("/",function(req, res){
-    res.render("index",{title:"首页"})
+    res.render("index",{title:"首页", books:{name:"node.js"}})
 });
 
 app.get("/reg",function(req, res){
