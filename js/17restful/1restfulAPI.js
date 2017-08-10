78806307be6e0cc8870a733e02b706485eb8e831
coyotena <shashahuo@163.com>
2017-08-10 11:54:20 +0800

    新增一个资源

--- js/17restful/1restfulAPI.js
+++ js/17restful/1restfulAPI.js
@@ -3,6 +3,7 @@
  */
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser")
 //存放所有的用户
 var users = [{id:1,name:"coyote"},{id:2,name:"coyote2"}];
 /**
@@ -12,7 +13,11 @@ var users = [{id:1,name:"coyote"},{id:2,name:"coyote2"}];
 app.set("view engine","ejs");
 //path.resolve 取当前目录的绝对路径
 app.set("views",__dirname);
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 //1.获取所有的用户  get/collection，collection数据数组，此处是users
+//请求地址     http://localhost:8080/users
 app.get("/users", function(req, res){
     //Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
     var accept = req.headers['accept'];//客户端需要的类型
@@ -38,7 +43,8 @@ app.get("/users", function(req, res){
     // res.send(users);
 
 });
-
+//2.返回单个用户
+//请求地址  http://localhost:8080/users/1
 app.get("/users/:id", function(req, res){
     var id = req.params.id;
     console.log(id);
@@ -47,6 +53,24 @@ app.get("/users/:id", function(req, res){
     });
     console.log(fileterdUsers.length > 0 ? fileterdUsers[0] : "此用户不存在");
     res.send(fileterdUsers.length > 0 ? fileterdUsers[0] : "此用户不存在");
+});
+//3.新增用户
+//请求地址  http://localhost:8080/users
+//-X 指定请求方法    --data 指定请求体数据
+//curl -X POST --data "name=coyote3" http://localhost:8080/users
+app.post("/users", function(req, res){
+    var addUser = req.body;
+    console.log(addUser);
+    if(addUser){
+        //为新增加的用户赋一个最大的id
+        addUser.id = users[users.length -1].id + 1;
+        users.push(addUser);
+        //当新增加一个资源的时候，要返回新生成的资源完整对象
+        res.send(addUser);
+        console.log(users);
+    }else{
+        res.send({msg:"增加资源失败"});
+    }
 });
 
 app.listen(8080);
